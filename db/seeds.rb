2445d155828ac08a0ccb76831c2324feb9ac64a8
ikona23 <ikona23@gmail.com>
2017-07-05 01:20:52 +1200

    books CRUD

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#books = Book.create([{ title: "Ruby on Rails 5: Web App Development for Beginners "},
+#                   ])
