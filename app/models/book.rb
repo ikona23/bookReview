2829cefd05de377c7d4264230bcca007161602f4
ikona23 <ikona23@gmail.com>
2017-07-18 23:05:03 +1200

    filling up the database

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,6 @@ class Book < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
 
-    has_attached_file :book_img, styles: { :book_index => "250x350>", :book_show => "325x475>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :book_img, styles: { :book_index => "250x350>", :book_show => "225x375>" }, default_url: "/images/:style/missing.png"
     validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\z/
   end
