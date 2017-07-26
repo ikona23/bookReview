7f7e1745fb361237beacccbb6699c7c85adaec41
ikona23 <ikona23@gmail.com>
2017-07-27 00:19:00 +1200

    welcome/home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/home'
+
   devise_for :users
   resources :books do
     resources :reviews
   end
-  root 'books#index'
+  root 'welcome#index'
 end
